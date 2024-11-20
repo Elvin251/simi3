3b66e474191f2914ba1ce0673014af808b489184
Elvin251 <emmmdov485@gmail.com>
2024-11-20 19:54:24 +0400

    d

---
+++ app.js
@@ -0,0 +1,8 @@
+let a = +prompt("birinci ededi daxil edin");
+let b = +prompt("ikinci ededi daxil edin");
+let c = +prompt("ucuncu ededi daxil edin");
+let cem =(a+b+c);
+let edediorta =(cem/3)
+
+
+document.write(edediorta)
